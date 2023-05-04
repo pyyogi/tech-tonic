e6538ba1773fa05a5609460ed1633711c2bb0ae1
True._.Mary <truefalsemary@gmail.com>
2023-05-04 21:44:55 +0300

    clear some files

--- src/main/java/com/kursach/repository/AutoblogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kursach.repository;
-
-import com.kursach.entity.Autoblog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutoblogRepository extends JpaRepository<Autoblog, Long> {
-}
